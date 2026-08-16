b9cd2f994ff21c0384e6cf87ba0a21ca662e50d3
praveenbhise07-art <praveenbhise07@gmail.com>
2026-08-15 23:10:37 -0400

    Fix connection string for PostgreSQL database

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 // Construct connection string if DB_CONNECTION_STRING is missing but individual components exist
-const connectionString = 'postgresql://psqladmin:Password123!@://psql-thoughtcanvas-dev.postgres.database.azure.com';
+const connectionString = 'postgresql://psqladmin:Password123!@psql-thoughtcanvas-dev.postgres.database.azure.com/thoughtcanvas';
 
 const pool = new Pool({
   connectionString: connectionString,
